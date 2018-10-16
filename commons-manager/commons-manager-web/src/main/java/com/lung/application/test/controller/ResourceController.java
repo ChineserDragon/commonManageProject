aa1a6c2c169659fd26e252a28fb03080b61f9ece
ChinPangLung <chinpang9527@gmail.com>
2018-10-16 17:17:18 +0800

    测试整合mybatis-plus代码生成器的代码

--- commons-manager/commons-manager-web/src/main/java/com/lung/application/test/controller/ResourceController.java
+++ commons-manager/commons-manager-web/src/main/java/com/lung/application/test/controller/ResourceController.java
@@ -1,10 +1,15 @@
 package com.lung.application.test.controller;
 
 
+import com.lung.application.test.api.IResourceService;
+import com.lung.application.test.entity.Resource;
+import com.lung.common.beans.ResultInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
 import com.lung.common.controller.SuperController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <p>
@@ -17,6 +22,18 @@ import com.lung.common.controller.SuperController;
 @Controller
 @RequestMapping("/resource")
 public class ResourceController extends SuperController {
+
+    @Autowired
+    IResourceService iResourceService;
+
+    @RequestMapping("testResource")
+    @ResponseBody
+    public ResultInfo TestResource() {
+
+        Resource resource = iResourceService.selectById(2);
+
+        return success(resource);
+    }
 
 }
 
